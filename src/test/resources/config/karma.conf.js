60ea2fbe4ecaf218f840f63587a69fa526a4c612
Florian Grundig <florian.grundig@gmx.de>
2013-09-04 08:33:24 +0200

    added package.json for easy installing karma and other required dependencies

--- src/test/resources/config/karma.conf.js
+++ src/test/resources/config/karma.conf.js
@@ -1,24 +1,33 @@
 module.exports = function (config) {
-  config.set({
-    basePath: '../../..',
+    config.set({
+        basePath: '../../..',
 
-    files: [
-      'main/webapp/lib/angular/angular.js',
-      'main/webapp/lib/angular/angular-*.js',
-      'test/javascript/lib/angular/angular-mocks.js',
-      'main/webapp/js/**/*.js',
-      'test/javascript/unit/**/*.js'
-    ],
+        files: [
+            'main/webapp/lib/angular/angular.js',
+            'main/webapp/lib/angular/angular-*.js',
+            'test/javascript/lib/angular/angular-mocks.js',
+            'main/webapp/js/**/*.js',
+            'test/javascript/unit/**/*.js'
+        ],
 
-    frameworks: ['jasmine'],
+        frameworks: ['jasmine'],
+        reporters: ['progress', 'junit'],
 
-    autoWatch: true,
+        autoWatch: false,
+        singleRun: true,
 
-    browsers: ['Chrome'],
+        browsers: ['Chrome'],
 
-    junitReporter: {
-      outputFile: '../target/test_out/unit.xml',
-      suite: 'unit'
-    }
-  });
+        junitReporter: {
+            outputFile: '../target/test_out/unit.xml',
+            suite: 'unit'
+        },
+        plugins: [
+            'karma-jasmine',
+            'karma-chrome-launcher',
+            'karma-junit-reporter'
+        ]
+
+
+    });
 };
